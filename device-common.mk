4cd0abfd3ed464beac0bf9543c7c89bfe90c1a51
Rocky7842 <49489159+Rocky7842@users.noreply.github.com>
2023-08-15 18:52:10 +0800

    [DNM] universal990: Drop UDFPS init script

--- device-common.mk
+++ device-common.mk
@@ -92,8 +92,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.samsung \
-    init.udfps.rc
+    android.hardware.biometrics.fingerprint@2.3-service.samsung
 
 # FlipFlap
 PRODUCT_PACKAGES += \
